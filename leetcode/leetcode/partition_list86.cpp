e900d00422a59ac0c77d0c4d519e84256693da81
zhoujianguo <zhoujianguowei@gmail.com>
2015-11-09 13:17:09 +0800

    finish partion list

--- leetcode/leetcode/partition_list86.cpp
+++ leetcode/leetcode/partition_list86.cpp
@@ -1,12 +1,27 @@
 #include"leetcode.h"
 ListNode* partition(ListNode* head, int x)
 {
-	ListNode *i = head, *tag = head;
-	while (i)
+	ListNode *greater = (ListNode*)(malloc(sizeof(ListNode))), *greaterTemp = greater;
+	ListNode *less = (ListNode*)(malloc(sizeof(ListNode))), *lessTemp = less;
+	greater->next = NULL;
+	less->next = NULL;
+	ListNode *iterator = head;
+	while (iterator)
 	{
-		
-		i = i->next;
+		if (iterator->val >= x)
+		{
+			greaterTemp->next = iterator;
+			greaterTemp = iterator;
+		}
+		else	if (iterator->val < x)
+		{
+			lessTemp->next = iterator;
+			lessTemp = iterator;
+		}
+		iterator = iterator->next;
 	}
+	lessTemp->next = greater->next;
+	head = less->next;
+	greaterTemp->next = NULL;
 	return head;
-
 }
